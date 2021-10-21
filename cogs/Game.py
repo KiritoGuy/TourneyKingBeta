370a9ce9bbb12be0be2a19106bfbdf21ee17d70d
Kirito Guy <69622567+QuantumGamerLive@users.noreply.github.com>
2021-10-21 19:40:46 +0530

    Update Game.py

--- cogs/Game.py
+++ cogs/Game.py
@@ -10,14 +10,15 @@ import random
 class Game(commands.Cog, name="play the game of death"):
     def __init__(self, bot: SquidGame):
         self.bot = bot
+        self.react_emoji = "✋"
 
     @commands.command(name="Start Game", help="Start a new Game")
     @slash_command(name="Start Game", help="Start a New Game")
     async def start_game(self, ctx: Union[commands.Context, InteractionContext]):
-        msg = await ctx.reply(embed=discord.Embed(title="Starting A New Game", description="Welcome To Squid Game. Creating a New Game...", color=discord.Color.red())).set_image(url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeXK3jXSdWsAMjTGuczb7dUhAsMnnnmXYnLw&usqp=CAU")
+        msg = await ctx.send(embed=discord.Embed(title="Starting A New Game", description="Welcome To Squid Game. Creating a New Game...", color=discord.Color.red())).set_image(url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeXK3jXSdWsAMjTGuczb7dUhAsMnnnmXYnLw&usqp=CAU")
         await asyncio.sleep(5)
-        sus = await msg.edit(embed = discord.Embed(title="Room Created", description="The Room for Squid Game has been Created! Now, Click on `✋` to Join The Room")
-        await sus.add_reaction(✋)
+        sus = await msg.edit(embed = discord.Embed(title="Room Created", description=f"The Room for Squid Game has been Created! Now, Click on `{self.react_emoji}` to Join The Room.")
+        await sus.add_reaction(self.react_emoji)
         new_msg = await channel.fetch_message(sus.id)
         users = await new_msg.reactions[0].users().flatten()
         users.pop(users.index(self.client.user))
