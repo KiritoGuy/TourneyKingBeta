514bede09db74ee99511ac6a38dfe73e0e288b08
Kirito Guy <69622567+QuantumGamerLive@users.noreply.github.com>
2021-10-21 18:15:25 +0530

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -9,6 +9,8 @@ class Database:
         self.db = self.cluster['brah']
         self.guild_data = self.db['guild_data']
         self.blacklist_data = self.db['blacklists']
+        self.player_data = self.db['players_ingame']
+        self.player_cache = []
         self.blacklist_cache = []
 
     async def get_guild_data(self, guild_id: int, raise_error: bool = True) -> Optional[dict]:
@@ -38,3 +40,18 @@ class Database:
         cursor = self.blacklist_data.find({})
         list_of_docs = await cursor.to_list(length=None)
         self.blacklist_cache = [e['_id'] for e in list_of_docs]
+
+    async def get_your_self_into_the_game(self, user_id: int, reason: str):
+        return await self.player_data.update_one(
+            filter={"_id": user_id},
+            update={"$set": {"reason": reason}},
+            upsert=True
+        )
+
+    async def remove_your_self_from_the_game(self, user_id: int):
+        return await self.player_data.delete_one({"_id": user_id})
+
+    async def get_your_self_info_the_game_cache(self):
+        cursor = self.player_data.find({})
+        list_of_docs = await cursor.to_list(length=None)
+        self.player_cache = [e['_id'] for e in list_of_docs]
