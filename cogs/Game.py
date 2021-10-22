c9aa1499c53d32d3a03aecd6d3d12c8f1858224d
Kirito Guy <69622567+KiritoGuy@users.noreply.github.com>
2021-10-22 08:20:03 +0530

    Update Game.py

--- cogs/Game.py
+++ cogs/Game.py
@@ -30,7 +30,7 @@ class Game(commands.Cog, name="play the game of death"):
                 return await ctx.send("no one reacted in time")
         overwrites = {user: discord.PermissionOverwrite(read_messages=True, send_messages=True) for user in users}
         overwrites[guild.default_role] = discord.PermissionOverwrite(read_messages=False, send_messages=False)
-        chnl = await ctx.guild.create_text_channel(f"Squid-Game", overwrites=overwrites) 
+        chnl = await ctx.guild.create_text_channel(f"Squid-Game", overwrites=overwrites)
 
 
 
